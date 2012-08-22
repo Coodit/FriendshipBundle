6a4208c56daa00bffa555fc5ef7fb354bb7e0084
Ph3nol <ph3@slynett.com>
2012-08-22 21:15:48 +0200

    Bundle initialization

---
+++ DependencyInjection/SlyFriendshipExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sly\FriendshipBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @uses Extension
+ * @author Cédric Dugat <ph3@slynett.com>
+ */
+class SlyFriendshipExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.xml');
+
+        $configuration = $configs[0];
+
+        /* --- Configuration management and overloads --- */
+
+        $container->setParameter('sly_friendship.config', $configuration);
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+SlyFriendshipBundle
+====================
+
+[![Continuous Integration status](https://secure.travis-ci.org/Slynett/FriendshipBundle.png)](http://travis-ci.org/Slynett/FriendshipBundle)
+
+This bundle allows you to build and manage friendships between your project users.
+
+**You are of course invited to [contribute](https://github.com/Slynett/FriendshipBundle/contributors) to this bundle! :)**
+
+## Documentation
+
+- **[Read Documentation](https://github.com/Slynett/FriendshipBundle/blob/master/Resources/doc/index.markdown)** (soon)
+
+## License
+
+This bundle is under the MIT license. See the complete license in the bundle:
+
+    Resources/meta/LICENSE

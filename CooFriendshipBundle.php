c4caccc4ce9e7504dbf440dbc2704db26099fccc
Ph3nol <ph3@slynett.com>
2012-08-25 13:26:38 +0200

    Better with main bundle class! :)

---
+++ CooFriendshipBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Coo\FriendshipBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * CooFriendshipBundle.
+ *
+ * @uses Bundle
+ * @author Cédric Dugat <ph3@slynett.com>
+ */
+class CooFriendshipBundle extends Bundle
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
